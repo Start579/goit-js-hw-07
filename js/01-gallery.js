4c0f0d322445619ef34535b12b5d0b51d0754364
Start579 <103378949+Start579@users.noreply.github.com>
2022-09-14 16:56:15 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Добавлена галлерея
+const gallery = document.querySelector(".gallery");
+const image = galleryItems.map(({preview, description, original}) => {
+return `<div class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`}).join("");
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", image); 
+// Подключен клик и запрещен переход по ссылке
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(evt) {
+const instance = basicLightbox.create(
+`<img src="${evt.target.dataset.source}" width="800" height="600">`)
+// Подключение Light Box "Открытие"
+instance.show()
+//Закрытие по Escape
+gallery.addEventListener("keydown", (evt) => {
+  if (evt.code === "Escape") {
+    instance.close();
+  }
+})
+    evt.preventDefault();
+        
+    if(evt.target.nodeName !== "IMG") {
+        return;
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// Добавлена галлерея
+const gallery = document.querySelector(".gallery");
+const image = galleryItems.map(({preview, description, original}) => {
+return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</div>`}).join("");
+
+gallery.insertAdjacentHTML("afterbegin", image); 
+// Подключен клик и запрещен переход по ссылке
+gallery.addEventListener("click", onGalleryClick);
+
+ function onGalleryClick(evt) {
+// const instance = basicLightbox.create(
+// `<img src="${evt.target.dataset.source}" width="800" height="600">`)
+// Подключение Light Box "Открытие"
+// instance.show()
+//Закрытие по Escape
+// gallery.addEventListener("keydown", (evt) => {
+//   if (evt.code === "Escape") {
+//     instance.close();
+//   }
+// })
+    evt.preventDefault();
+        
+    if(evt.target.nodeName !== "IMG") {
+        return;
+    }
+}
 console.log(galleryItems);
