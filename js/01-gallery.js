19c8e618b4efca15ae3578d4c73b0dd02fd0b48b
Start579 <103378949+Start579@users.noreply.github.com>
2022-09-16 12:08:02 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items.js';
 // Добавлена галлерея
 const gallery = document.querySelector(".gallery");
 const image = galleryItems.map(({preview, description, original}) => {
-return `<div class="gallery__item">
+return `<li class="gallery__item">
   <a class="gallery__link" href="${preview}">
     <img
       class="gallery__image"
@@ -12,26 +12,18 @@ return `<div class="gallery__item">
       alt="${description}"
     />
   </a>
-</div>`}).join("");
+</li>`}).join("");
 
 gallery.insertAdjacentHTML("beforeend", image); 
 // Подключен клик и запрещен переход по ссылке
 gallery.addEventListener("click", onGalleryClick);
 
 function onGalleryClick(evt) {
-const instance = basicLightbox.create(
-`<img src="${evt.target.dataset.source}" width="800" height="600">`)
-// Подключение Light Box "Открытие"
-instance.show()
-//Закрытие по Escape
-gallery.addEventListener("keydown", (evt) => {
-  if (evt.code === "Escape") {
-    instance.close();
-  }
-})
-    evt.preventDefault();
-        
+    evt.preventDefault();   
     if(evt.target.nodeName !== "IMG") {
         return;
-    }
+    }  
+const instance = basicLightbox.create(
+`<img src="${evt.target.dataset.source}" width="800" height="600">`)
+instance.show()   
 }
